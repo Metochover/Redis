20f32fa18922c362c20b613876e29bb2f623baee
metehan <33metehan23@gmail.com>
2024-12-25 17:27:01 +0300

    In-MemoryCache

---
+++ CacheExamplesFirst/Distributed.Cache/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+
+namespace Distributed.Cache.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        IDistributedCache _distiributedCache;
+        public HomeController(IDistributedCache distributedCache)
+        {
+                _distiributedCache = distributedCache;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Set(string name , string surname)
+        {
+            await _distiributedCache.SetStringAsync("name", name, options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(30),
+            });
+            await _distiributedCache.SetAsync("surname", Encoding.UTF8.GetBytes(surname), options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(30),
+            });
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var name = await _distiributedCache.GetStringAsync("name");
+            var surnameBinary = await _distiributedCache.GetAsync("surname");
+            var surname = Encoding.UTF8.GetString(surnameBinary);
+
+            return Ok(name+" "+surname);
+        }
+    }
+}
---
+++ CacheExamplesFirst/InMemory.Cache/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System.Xml.Linq;
+
+namespace InMemory.Cache.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        readonly IMemoryCache _memoryCache;
+        public HomeController(IMemoryCache memoryCache)
+        {
+               _memoryCache = memoryCache;
+        }
+
+        [HttpPost]
+        public void SetName(string name)
+        {
+            _memoryCache.Set("name", name);  //cache'e "key-value" formatında veri set ediyoruz.
+        }
+
+        [HttpGet("Get")]
+        public string GetName()
+        {
+            return _memoryCache.Get<string>("name"); //cache'de "key"'i "name" olan veriyi okumaya çalışıyoruz
+        }
+
+        [HttpGet("TryGetValue")]
+        public string TryGetName()
+        {
+            if (_memoryCache.TryGetValue<string>("name", out string name))//cache'de "key"'i "name" olan veriyi okumaya çalışıyoruz burada boolean bir değer döne bize
+            {
+                return name.Substring(2);
+            }
+            else
+                return null;
+        }
+
+        [HttpPost("setDate")]
+        public void SetDate()
+        {
+            _memoryCache.Set("date", DateTime.Now, options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+        }
+
+        [HttpGet("GetDate")]
+        public DateTime GetDate()
+        {
+            return _memoryCache.Get<DateTime>("date"); 
+        }
+    }
+}
